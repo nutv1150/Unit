6cfb1405a61dfc03bf3256a6b4ab0e019a7ec598
nut_y5 <nut0613152926@gmail.com>
2026-02-03 17:07:45 +0700

    Update data_hash.py

--- pages/data_hash.py
+++ pages/data_hash.py
@@ -4,7 +4,7 @@ class DataHashPage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         
-        # เก็บสถานะปัจจุบัน
+        # เก็บสถานะปัจจุบัน 112233
         self.mode = "Decode"  # ค่าเริ่มต้นคือ Decode
         self.tab_buttons = {} # เก็บ object ของปุ่ม Tab
 
